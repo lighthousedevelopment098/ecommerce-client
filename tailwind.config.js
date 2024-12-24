19f6ea58d8a402af889de7b3b9e5ded6e1100b09
Faisal Khan <faisalkh01122@gmail.com>
2024-12-24 05:51:49 -0800

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,39 @@ import withMT from '@material-tailwind/react/utils/withMT'
 
 const { colors: defaultColors } = require('tailwindcss/defaultTheme')
 
+// primary: {
+// 			700: "#60792a",
+// 			600: "#708e31",
+// 			500: "#a1cb46",
+// 			400: "#90b63f",
+// 			300: "#c6e198",
+// 			200: "#e2f0cc",
+// 			100: "#e2efc7",
+// 		}
 const colors = {
     ...defaultColors,
     ...{
+        // primary: {
+        //     700: '#10430D', // Darker shade of #1D6713
+        //     600: '#165310', // Slightly darker shade
+        //     500: '#1D6713', // Main color
+        //     400: '#25851B', // Lighter shade
+        //     300: '#36A92C', // Even lighter
+        //     200: '#8DDA87', // Lighter, softer tone
+        //     100: '#C2EEC3', // Lightest shade
+        // },
+
         primary: {
-            50: '#e6f7ed',
-            100: '#ccefdc',
-            200: '#99dfb9',
-            300: '#66cf97',
-            400: '#009444', // main color
-            500: '#00843e',
-            600: '#007338',
-            700: '#006332',
-            800: '#00532c',
-            900: '#004326',
+            50: '#A1CA46',
+            100: '#A1CA46',
+            200: '#A1CA46',
+            300: '#A1CA46',
+            400: '#96bf39', // main color
+            500: '#648c1b',
+            600: '#A1CA46',
+            700: '#A1CA46',
+            800: '#A1CA46',
+            900: '#A1CA46',
         },
     },
 }
